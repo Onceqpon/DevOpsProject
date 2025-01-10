771ada005b766a231a063f74813afa4fb18d112f
Onceqpon <117514577+Onceqpon@users.noreply.github.com>
2025-01-10 16:23:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ def hello_name(name):
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/info')
+def info():
+    return jsonify({
+        "app": "Flask Web App",
+        "version": "1.0",
+        "author": "Your Name"
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
